d9f14ccf33aa62ebb055ebcd2edb1099ae4fe0af
Will Binns-Smith <wbinnssmith@gmail.com>
2026-01-29 20:11:58 -0800

    Gate server HMR behind experimentalServerFastRefresh flag

--- packages/next/src/server/lib/router-utils/setup-dev-bundler.ts
+++ packages/next/src/server/lib/router-utils/setup-dev-bundler.ts
@@ -115,6 +115,7 @@ export type SetupOpts = {
   port: number
   onDevServerCleanup: ((listener: () => Promise<void>) => void) | undefined
   resetFetch: () => void
+  experimentalServerFastRefresh?: boolean
 }
 
 export interface DevRoutesManifest {
@@ -236,7 +237,8 @@ async function startWatcher(
           serverFields,
           distDir,
           resetFetch,
-          lockfile
+          lockfile,
+          opts.experimentalServerFastRefresh
         )
       })()
     : await (async () => {
